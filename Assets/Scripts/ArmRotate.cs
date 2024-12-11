407005133d7c7e4e4c67aa7e1a7105901bae5d38
Gemnz29 <167627614+Gemnz29@users.noreply.github.com>
2024-12-12 00:12:15 +0700

    PointTowardMouse
    help me DK

---
+++ Assets/Scripts/ArmRotate.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointTowardMouse : MonoBehaviour
+{
+    Camera cam;
+    Vector2 MousePos
+    {
+        get
+        {
+            Vector2 pos = cam.ScreenToWorldPoint(Input.mousePosition);
+            return pos;
+        }
+    }
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        Vector2 dir = (Vector2)transform.position - MousePos;    
+        float angle = (Mathf.Atan2(dir.y,dir.x)*Mathf.Rad2Deg);
+        transform.eulerAngles= new Vector3(0f,0f,angle+180f);
+    }
+
+}
